64fa487ea6f86b1bb832a22217dacfae9112e0f2
Clemens Felber <clemens.felber@emerson.com>
2025-06-19 15:52:42 +0200

    python: applied ni-python-styleguide
    - applied black formatting rules
    - resolved flake8 linter issues
    - replaced settings objects by named tuples
    - fixed runtime error messages in setup_graph

--- check-sw-env/check-sw-env.py
+++ check-sw-env/check-sw-env.py
@@ -4,17 +4,19 @@
 #
 # SPDX-License-Identifier: MIT
 #
-""""Example for verifying the UHD software environment.
+"""Example for verifying the UHD software environment.
 
 This example checks if the UHD Python module is installed and working correctly.
 It queries the UHD version and lists any connected USRP devices.
 This is useful for ensuring that the UHD software environment is set up correctly.
-"""	
+"""
+
 
 def check_sw_env():
     """Check the UHD software environment by reading out the version."""
     try:
         import uhd
+
         # Query UHD version information
         version = uhd.get_version_string()
         # Query USRP devices infromation
@@ -25,14 +27,16 @@ def check_sw_env():
         else:
             for usrp in usrps:
                 if usrp:
-                    usrps_string += (usrp.to_pp_string())
-        return("UHD is installed and working in the Python environment.\n"
-               f"Version: {version}\n\n"
-               f"Found USRP Devices (multiple occurrences of the same device possible)\n\n{usrps_string}")
+                    usrps_string += usrp.to_pp_string()
+        return (
+            "UHD is installed and working in the Python environment.\n"
+            f"Version: {version}\n\n"
+            f"Found USRP Devices (multiple occurrences of the same device possible)\n\n{usrps_string}"
+        )
     except ModuleNotFoundError as err:
-        return ("UHD is not installed or not found in the Python environment. \n"
-                f"Error: {err}")
-    
+        return "UHD is not installed or not found in the Python environment. \n" f"Error: {err}"
+
+
 if __name__ == "__main__":
     result = check_sw_env()
     print(result)
